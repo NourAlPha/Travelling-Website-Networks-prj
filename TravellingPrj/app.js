e180bf50102165c9e230591273dab4f95d621bd0
Nour Eldien Ayman <49641430+NourAlPha@users.noreply.github.com>
2022-12-10 22:42:42 +0200

    Created the project

---
+++ TravellingPrj/app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var path = require('path');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res){
+    res.render('index', {title: "Express"});
+});
+
+app.listen(3000);
